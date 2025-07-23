cbcdb8c60e65f04a2a927cabed729cc14a892e59
ONURAL OSMAN KALAYCI <64443163+onuralklyc@users.noreply.github.com>
2025-07-23 13:55:51 +0300

    app.py

--- app.py
+++ app.py
@@ -81,7 +81,11 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
+import os
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
